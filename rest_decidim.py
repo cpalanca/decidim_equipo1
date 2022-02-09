bb3c7aa640ee7904ec200c6f9f72cac158c01e00
Carlos Palanca <palanca_carlos@hotmail.com>
2022-02-09 21:51:33 +0100

    FINISHED APIFAST IN PYTHON

--- rest_decidim.py
+++ rest_decidim.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-import datetime
+from datetime import datetime
 from typing import Optional
 import sqlite3
 from fastapi.encoders import jsonable_encoder
@@ -25,7 +25,7 @@ async def select_all():
     for record in records:
         authors.append( dict( zip( columnNames , record ) ) )
     json_compatible_item_data = jsonable_encoder(authors)
-        
+
     return JSONResponse(content=json_compatible_item_data)
 
 
@@ -93,11 +93,11 @@ class Comments(BaseModel):
     id: int
     id_authors: int
     body: str
-    id_comments: int
-    created_at: datetime.date
+    id_comments: str
+    created_at: datetime
     down_votes: int
     up_votes: int
-    reply_to: int
+    reply_to: str
     reply_level: int
 
 @app.get("/comments")
@@ -152,11 +152,11 @@ async def delete_with_condition(id: int):
 async def insert(item: Comments):
     connection = sqlite3.connect('/home/tsi/equipo1/decidim_equipo1.db')
     cursor = connection.cursor()
-
-    new_comment = (item.id, item.id_authors, item.body, item.id_comments, item.created_at, item.down_votes, item.up_votes, item.reply_to, item.reply_level)
+    
     try:
-        cursor.execute("INSERT INTO comments(id, id_authors, body, id_comments, created_at, down_votes, up_votes, reply_to, reply_level) VALUES(?,?,?,?,?,?,?,?,?)", (new_comment))
-        records = cursor.fetchlast()
+        cursor.execute("INSERT INTO comments(id, id_authors, body, id_comments, created_at, down_votes, up_votes, reply_to, reply_level) \
+        VALUES(?,?,?,?,?,?,?,?,?)", (item.id, item.id_authors, item.body, item.id_comments, item.created_at, item.down_votes, item.up_votes, item.reply_to, item.reply_level))
+
         cursor.close()
         connection.commit()
         return {"message": "El Comentario se ha guardado correctamente."}
@@ -167,12 +167,15 @@ async def insert(item: Comments):
 async def update(id: int, item: Comments):
     connection = sqlite3.connect('/home/tsi/equipo1/decidim_equipo1.db')
     cursor = connection.cursor()
-    new_comment = (item.id, item.id_authors, item.body, item.id_comments, item.created_at, item.down_votes, item.up_votes, item.reply_to, item.reply_level)
+    #print(item.created_at)
     try:
-        cursor.execute("UPDATE comments SET id_authors = ?, body = ?, id_comments = ?, created_at = ?, down_votes = ?, up_votes = ?, reply_to = ?, reply_level = ? WHERE id = ?", (new_comment, id))
+        cursor.execute("UPDATE comments SET id_authors = ?, body = ?, id_comments = ?, created_at = ?, down_votes = ?, up_votes = ?, reply_to = ?, reply_level = ? \
+        WHERE id = ?", (item.id_authors, item.body, item.id_comments, item.created_at, item.down_votes, item.up_votes, item.reply_to, item.reply_level, id))
+        
         cursor.close()
         connection.commit()
-        json_compatible_item_data = jsonable_encoder(new_comment)
+
+        json_compatible_item_data = jsonable_encoder(item)
         return JSONResponse(content=json_compatible_item_data)
     except:
         return HTTPException(status_code=404, detail="Comentario not found")
@@ -183,8 +186,8 @@ class Proposal(BaseModel):
     id_authors: int
     id_comments: str
     n_endorsement: int
-    created_at: datetime.date
-    published_at: datetime.date
+    created_at: datetime
+    published_at: datetime
 
 @app.get("/proposal")
 async def select_all():
@@ -239,30 +242,30 @@ async def insert(item: Proposal):
     connection = sqlite3.connect('/home/tsi/equipo1/decidim_equipo1.db')
     cursor = connection.cursor()
 
-    new_proposal= (item.id, item.title, item.id_authors, item.id_comments, item.n_endorsement, item.created_at, item.published_at)
     try:
-        cursor.execute("INSERT INTO comments(id, title, id_authors, id_comments, endorsement, created_at, published_at) VALUES(?,?,?,?,?,?,?)", (new_proposal))
-        records = cursor.fetchall()
+        cursor.execute("INSERT INTO proposal(id, title, id_authors, id_comments, n_endorsement, created_at, published_at) \
+        VALUES(?,?,?,?,?,?,?)", (item.id, item.title, item.id_authors, item.id_comments, item.n_endorsement, item.created_at, item.published_at))
+        
         cursor.close()
         connection.commit()
         return {"message": "La propuesta ha sido creada correctamente"}
     except:
         return HTTPException(status_code=404, detail="Ya exite una propuesta con esa ID, revisa los datos")
+        
 
 @app.put("/proposal/{id}")
 async def update(id: int, item: Proposal):
     connection = sqlite3.connect('/home/tsi/equipo1/decidim_equipo1.db')
     cursor = connection.cursor()
     
-    new_proposal= (item.id, item.title, item.id_authors, item.id_comments, item.n_endorsement, item.created_at, item.published_at)
     try:
-        cursor.execute("UPDATE proposal SET title = ?, id_authors = ?, id_comments = ?, endorsement = ?, created_at = ?, published_at = ? WHERE id = ?", (new_proposal, id))
+        cursor.execute("UPDATE proposal SET title = ?, id_authors = ?, id_comments = ?, n_endorsement = ?, created_at = ?, published_at = ? \
+        WHERE id = ?", (item.title, item.id_authors, item.id_comments, item.n_endorsement, item.created_at, item.published_at, id))
+
         cursor.close()
         connection.commit()
-        json_compatible_item_data = jsonable_encoder(new_comment)
+
+        json_compatible_item_data = jsonable_encoder(item)
         return JSONResponse(content=json_compatible_item_data)
     except:
         return HTTPException(status_code=404, detail="Propuesta not found")
-
-
-
